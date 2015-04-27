ed00dfe6b2f3e204c2c5c0b0a490307864f1efbb
wheatdog <thenormalgod@gmail.com>
2015-04-28 00:58:12 +0800

    implement template-doc folder

--- new.sh
+++ new.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 nextID=$(( $(find . -maxdepth 1 -type d -name 'hw*' | wc -l) + 1 ))
+
 echo "Create hw${nextID}..."
-mkdir hw${nextID} hw${nextID}/code hw${nextID}/doc
+mkdir hw${nextID} hw${nextID}/code 
 cp dep/Makefile hw${nextID}/code
+cp -r dep/doc-template hw${nextID}/doc
 
