f0557060504b96c85fa24867f4cbe138983b279f
wheatdog-nb-fedora <thenormalgod@gmail.com>
2015-05-24 22:02:24 +0800

    hast test about written part bonus

--- hw5/code/hashtest.cpp
+++ hw5/code/hashtest.cpp
@@ -1,62 +1,97 @@
 #include <cstring>
 #include <iostream>
+#include <vector>
+
+#define Size(A) sizeof(A) / sizeof(*(A))
+
+const int UP = 20;
+const int SOMETHING = 6;
 
 using namespace std;
 
 int HashCode(char *s)
 {
     int Result;
-    Result = (7 * strlen(s) + 2 * s[0] + 0 * s[strlen(s) - 1])  % 32;
+    Result = (strlen(s) + s[0] + s[strlen(s) - 1]);
     return Result;
 }
 
-int CheckUnique(int *array)
+#if 0
+int Generate(int index, char *Keywords[], bool isUsed[],
+             vector<int> &table)
 {
-    int Count = 0;
-    for (int i = 0; i < 32; i++)
-        for (int j = 0; j < i; j++)
-            if (array[j] == array[i])
-                Count++;
-    return Count;
+    if (index == 26)
+        return 1;
+
+    for (int count = 0; count < UP; count++)
+    {
+        table[index] = count;
+        int conflict = 0;
+        for (int wordCount = 0; wordCount < 32 && !conflict; wordCount++)
+        {
+            HashValue = HashCode(Keywords[wordCount], table);
+            if (HashValue < 0 || isUsed[HashValue])
+                conflict = 1;
+        }
+
+        if (!conflict && Generate(index + 1, Keywords, isUsed, table))
+            return 1;
+    }
+}
+#endif
+
+void Check(char *Keywords[], int TotalNum)
+{
+    int start_table[26] = {};
+    for (int count = 0; count < TotalNum; count++)
+        start_table[Keywords[count][0] - 'a']++;
+
+    int end_table[26] = {};
+    for (int count = 0; count < TotalNum; count++)
+        end_table[Keywords[count][strlen(Keywords[count]) - 1] - 'a']++;
+
+    for (int count = 0; count < 26; count++)
+        cout << (char)('a' + count) << ": " << start_table[count] 
+            << "\t" << end_table[count] 
+            << "\t" << start_table[count] + end_table[count]<< endl;
+    return;
 }
 
 int main()
 {
-    char *Keyword[] = {"auto",
-                       "break",
+    char *Keywords[] = {"else", "enum", "extern",
+                       "short", "signed", "sizeof", "static", "struct", "switch",
                        "case", "char", "const", "continue",
                        "default", "do", "double",
-                       "else", "enum", "extern",
-                       "float", "for",
-                       "goto",
-                       "if", "int",
-                       "long", 
-                       "register", "return",
-                       "short", "signed", "sizeof", "static", "struct", "switch",
-                       "typedef",
                        "union", "unsigned",
+                       "float", "for",
+                       "register", "return",
                        "void", "volatile",
+                       "if", "int",
+                       "auto",
+                       "break",
+                       "goto",
+                       "long", 
+                       "typedef",
                        "while"};
 
-    int TotalNum = sizeof(Keyword) / sizeof(*Keyword);
-    int HashCodeArray[TotalNum];
 
-    cout << "Total Num = " << TotalNum << endl;
-    for (int i = 0; i < TotalNum; i++)
-    {
-        HashCodeArray[i] = HashCode(Keyword[i]);
-        cout << Keyword[i] << ": " <<  HashCodeArray[i] << endl;
-    }
+#if 0
 
-    int Count = CheckUnique(HashCodeArray);
-    if (Count == 0)
-    {
-        cout << "Nice HashCode!" << endl;
-    }
-    else
-    {
-        cout << ":( " << Count << endl;
-    }
+    vector<int> table(26, 0);
+    bool isUsed[26] = {};
 
+    cout << Size(Keywords) << endl;
+    Check(Keywords, Size(Keywords));
+
+    Generate(0, Keywords, isUsed, table);
+    cout << "Generate Success!" << endl;
+
+    for (int i = 0; i < 26; i++)
+        cout << table[i] << endl;
+#endif
+
+    for (unsigned int i = 0; i < Size(Keywords); i++)
+        cout << HashCode(Keywords[i]) << endl;
     return 0;
 }
